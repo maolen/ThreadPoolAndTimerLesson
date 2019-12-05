e2b3113722fde1481011ff07278b6b035ed7023e
Kamila Zhakupova <zhakupova.kamila@gmail.com>
2019-12-05 19:52:45 +0600

    Add project files.

---
+++ ThreadPoolLesson/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadPoolLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadPool.GetAvailableThreads(out int threads, out int ports);
+            Console.WriteLine(threads);
+            ThreadPool.QueueUserWorkItem(ProcessNumbers);
+            Thread.CurrentThread.Join();
+        }
+
+        static void ProcessNumbers(object state)
+        {
+            var currentThread = Thread.CurrentThread;
+
+            for (var i = 0; i <= 10; i++)
+            {
+                Console.WriteLine($"[{currentThread.ManagedThreadId}] - {i}");
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
